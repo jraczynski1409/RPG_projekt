5a22bd53319e2e32091de72af6e1167c0c7ce9b9
Jakub Raczyński <95698623+jraczynski1409@users.noreply.github.com>
2022-10-19 20:58:39 +0200

    Update po 1 lab
    Uzupełnione chodzenie o zegary.

--- engine.cpp
+++ engine.cpp
@@ -1,121 +1,125 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
-
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
 
 using namespace sf;
 
+#define BLOCKSIZE 64;
+
 class Engine {
+private:
+	int windowWidth = 1368;
+	int windowHeight = 768;	
+	int FPS = 60;
+	Clock clock;
+
+
 public:
-	static const int mapWidth = 5;
-	static const int mapHeight = 5;
-	static const int blockSize = 64;
-	static const int windowWidth = 1368;
-	static const int windowHeight = 720;
-	
-	int isRun = 1;
-
-	Engine() {
-
-	}
-
-	Sprite makeSprite(String path);
-	
-};
-
-Sprite Engine::makeSprite(String path)
-{
-	Texture x;
-	x.loadFromFile(path);
-	Sprite a;
-	a.setTexture(x);
-	return a;
-}
-
-
-
-
-
-class Player {
-public:
-	String name;
-	int lvl;
-	Sprite outfit;
-
-	int positionX;
-	int positionY;
-
-	Player(String name, int lvl, Sprite outfit)
+	int getWindowWidth()
 	{
-		this->name = name;
-		this->lvl = lvl;
-		this->outfit = outfit;
+		return windowWidth;
+	}
 
-		this->positionX = 64;
-		this->positionY = 64;
+	int getWindowHeight()
+	{
+		return windowHeight;
+	}
+
+	void setWindowWidth(int windowWidht)
+	{
+		this->windowWidth=windowWidth;
+	}
+
+	void setWindowHeight(int windowHeight)
+	{
+		this->windowHeight=windowHeight;
+	}
+
+	int getFPS()
+	{
+		return FPS;
+	}
+
+	void setFPS(int FPS)
+	{
+		this->FPS = FPS;
+	}
+	
+	
+	int game()
+	{
+		RenderWindow window(VideoMode(windowWidth, windowHeight), "TEST");
+		window.setFramerateLimit(FPS);
+
+		Clock clock;	
+
+		Texture woodTX;
+		woodTX.loadFromFile("wood_floor.jpg");
+		Sprite wood;
+		wood.setTexture(woodTX);
+
+		int woodX =512;
+		int woodY =512;
+		wood.setPosition(woodX, woodY);
+		
+
+		while (window.isOpen()) {
+
+			Time timer = clock.getElapsedTime();
+			float delay = timer.asSeconds();
+
+			std::cout << delay << std::endl;
+
+			if ((Keyboard::isKeyPressed(Keyboard::Escape)))
+			{
+				window.close();
+			}
+
+			if (delay > 0.3f)
+			{
+				if ((Keyboard::isKeyPressed(Keyboard::W)))
+				{
+					woodY = woodY - 64;
+					wood.setPosition(woodX, woodY);
+					clock.restart();
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::S))
+				{
+					woodY = woodY + 64;
+					wood.setPosition(woodX, woodY);
+					clock.restart();
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::A))
+				{
+					woodX = woodX - 64;
+					wood.setPosition(woodX, woodY);
+					clock.restart();
+				}
+				else if (Keyboard::isKeyPressed(Keyboard::D))
+				{
+					woodX = woodX + 64;
+					wood.setPosition(woodX, woodY);
+					clock.restart();
+				}
+			}
+			
+			
+
+
+			window.draw(wood);
+			window.display();
+			window.clear(Color::Black);
+		}
+		return 0;
 	}
 };
-
-
-
-
-
 
 
 
 int main()
 {
-
-
-	Engine engine;
-	RenderWindow window(VideoMode(engine.windowWidth, engine.windowHeight), "TEST");
-	window.setFramerateLimit(1);
-
-
-	Sprite wood = engine.makeSprite("wood_floor.jpg");
-
-	Player player("K", 1, wood);
-	
-	Clock c1;
-	c1.getElapsedTime();
-	
-	Time t1;
-	t1.asMilliseconds();
-
-	while ((window.isOpen())) {
-
-		
-			
-			if (Keyboard::isKeyPressed(Keyboard::W))
-			{
-				player.positionY = player.positionY - 64;
-				player.outfit.setPosition(player.positionX, player.positionY);
-				
-				
-			}
-			else if (Keyboard::isKeyPressed(Keyboard::S))
-			{
-				player.positionY = player.positionY + 64;
-				player.outfit.setPosition(player.positionX, player.positionY);
-
-			}
-			else if (Keyboard::isKeyPressed(Keyboard::A))
-			{
-				player.positionX = player.positionX - 64;
-				player.outfit.setPosition(player.positionX, player.positionY);
-
-			}
-			else if (Keyboard::isKeyPressed(Keyboard::D))
-			{
-				player.positionX = player.positionX + 64;
-				player.outfit.setPosition(player.positionX, player.positionY);
-
-			}
-
-		window.draw(player.outfit);
-		window.display();
-
-		window.clear(Color::Black);
-	}
+	Engine game;
+	game.game();
 
 }
-
